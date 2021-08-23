074f429fdc36b4b324f14330ee329d76335bfa50
Andreas Rozek <A.Rozek@gmx.de>
2021-08-23 09:27:46 +0200

    setup build

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+// see https://github.com/rozek/build-configuration-study
+
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: './src/reusable-flows.ts',
+  output: [
+    {
+      file:     './javascript-function/reusable-flows.js',
+      format:    'cjs',
+      name:      'reusable-flows',
+      noConflict:true,
+      sourcemap: true,
+      exports:   'auto',
+      plugins:   [terser({ format:{ comments:false, safari10:true } })],
+    }
+  ],
+  plugins: [
+    resolve(), commonjs(), typescript()
+  ],
+};
